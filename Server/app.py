2153d35b53762f16a6d1e209656954cf57935201
xxwikkixx <wikkii@msn.com>
2019-01-31 01:01:01 -0500

    file uplader filter

--- Server/app.py
+++ Server/app.py
@@ -23,16 +23,27 @@ def uploadPage():
     return render_template('uploadPage.html')
 
 
+
+# https://github.com/ngoduykhanh/flask-file-uploader
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
 UPLOAD_FOLDER = 'uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file[]']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('upload_file'))
-    return 'OK'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file'))
+            return 'OK'
+        else:
+            return "File Extension not allowed"
+
 
 
 
