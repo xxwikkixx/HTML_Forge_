a071bbe2ebe44a58ddd8bb372df6590f80220269
xxwikkixx <wikkii@msn.com>
2019-01-30 22:40:49 -0500

    html pages

--- Server/app.py
+++ Server/app.py
@@ -4,9 +4,20 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def mainPage():
     return render_template('index.html')
 
+@app.route('/processing')
+def processingPage():
+    return render_template('processingPage.html')
+
+@app.route('/results')
+def resultsPage():
+    return render_template('resultsPage.html')
+
+@app.route('/upload')
+def uploadPage():
+    return render_template('uploadPage.html')
 
 if __name__ == '__main__':
     app.run()
