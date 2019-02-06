dd92ce22bea6dfbed205949cb1ee6eb5ff32883d
xxwikkixx <wikkii@msn.com>
2019-02-06 16:27:42 -0500

    Image OCR

--- Server/src/ImageOCR/ImageOCR.py
+++ Server/src/ImageOCR/ImageOCR.py
@@ -1,5 +1,20 @@
 import os
+import io
+from google.cloud import vision
+from google.cloud.vision import types
 
+client = vision.ImageAnnotatorClient()
 
-class ImageOCR():
-    def __init__(self):
+path = '/uploadBlocks/'
+
+def detect_props(path):
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content = content)
+
+    response = client.image_properties(image=image)
+
+    props = response.image_properties_annotation
+
+    
