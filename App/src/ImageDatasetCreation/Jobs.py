593ee0c43bb87cdaf63a6f6ff57d7e2833aa507d
Hul233 <hul233@psu.edu>
2019-03-01 14:31:10 -0500

    Image Gallary Preview Added to the job queue

--- App/src/ImageDatasetCreation/Jobs.py
+++ App/src/ImageDatasetCreation/Jobs.py
@@ -2,11 +2,16 @@ import itertools
 import random
 from PIL import Image
 
-from ImageDatasetCreation import checkAllRescale, findCenterXY, ImageCons, dirFiles
+from MergeTool import dirFiles, checkAllRescale, findCenterXY, scaleByRatio
+from ImageCons import ImageCons
+
+
+# from App.src.ImageDatasetCreation.MergeTool import scaleByRatio
 
 
 def structLeftImageBox():
-    session = [dirFiles("OuterBox"), dirFiles("Image"), dirFiles("Text")]
+    # session = [dirFiles("OuterBox"), dirFiles("Image"), dirFiles("Text")]
+    session = [dirFiles("OuterBox"), dirFiles("PersonImage"), dirFiles("Text")]  # Person Images
     que_List = list(itertools.product(*session))
     # print(que_List)
 
@@ -39,7 +44,8 @@ def structLeftImageBox():
 
 
 def structRightImageBox():
-    session = [dirFiles("OuterBox"), dirFiles("Image"), dirFiles("Text")]
+    # session = [dirFiles("OuterBox"), dirFiles("Image"), dirFiles("Text")]
+    session = [dirFiles("OuterBox"), dirFiles("PersonImage"), dirFiles("Text")]  # Person Images
     que_List = list(itertools.product(*session))
     # print(que_List)
 
@@ -72,7 +78,8 @@ def structRightImageBox():
 
 
 def structCenterImageBox():
-    session = [dirFiles("OuterBox"), dirFiles("Image"), dirFiles("Text")]
+    # session = [dirFiles("OuterBox"), dirFiles("Image"), dirFiles("Text")]
+    session = [dirFiles("OuterBox"), dirFiles("PersonImage"), dirFiles("Text")]  # Person Images
     que_List = list(itertools.product(*session))
     # print(que_List)
 
@@ -103,6 +110,7 @@ def structCenterImageBox():
         image_copy.paste(Text, findCenterXY(temp.CENTER_BOTTOM, temp))
 
         image_copy.save("Output/IMG_Top_Text_Bottom/" + str(i) + ".png")
+
 
 def plainImageGallary():
     session = [dirFiles("OuterBox"), dirFiles("Image")]
@@ -136,12 +144,131 @@ def plainImageGallary():
         image_copy.paste(IMG, findCenterXY(temp.TOP_RIGHT, temp))
         image_copy.paste(IMG, findCenterXY(temp.RIGHT, temp))
         image_copy.paste(IMG, findCenterXY(temp.BOTTOM_RIGHT, temp))
-
-
-
-
         image_copy.save("Output/Plain_Image_Gallary/" + str(i) + ".png")
 
-plainImageGallary()
-# structCenterImageBox()
+
+# Image Flip
+
+def imageFlip():
+    session = [dirFiles("OuterBox"), dirFiles("Image"), dirFiles("LeftArrow"), dirFiles("RightArrow")]
+    que_List = list(itertools.product(*session))
+    # print(que_List)
+
+    for i in range(0, 200):
+        print(i)
+        rand = random.randint(1, len(que_List))
+
+        try:
+            OuterBox = Image.open(que_List[rand][0])
+            IMG = Image.open(que_List[rand][1])
+            LeftArrow = Image.open(que_List[rand][2])
+            RightArrow = Image.open(que_List[rand][3])
+        except IOError:
+            print("Error")
+
+        # Initial the Outer Box
+        temp = ImageCons(OuterBox.size)
+        temp.setBase(OuterBox.size[0], OuterBox.size[1])
+        OuterLayer = OuterBox.copy()
+
+        # Process IMG
+        temp.setTarget(IMG.size[0], IMG.size[1])
+        # IMG = scaleByRatio(IMG, temp, 0.6)
+        IMG = checkAllRescale(IMG, temp)
+        OuterLayer.paste(IMG, findCenterXY(temp.CENTER, temp))
+
+        print(LeftArrow.size)
+        temp.setTarget(LeftArrow.size[0], LeftArrow.size[1])
+        LeftArrow = checkAllRescale(LeftArrow, temp)
+        OuterLayer.paste(LeftArrow, findCenterXY(temp.LEFT, temp))
+
+        print(LeftArrow.size)
+        temp.setTarget(RightArrow.size[0], RightArrow.size[1])
+        RightArrow = checkAllRescale(RightArrow, temp)
+        OuterLayer.paste(RightArrow, findCenterXY(temp.RIGHT, temp))
+
+        # temp.setTarget(Text.size[0], Text.size[1])
+        # Text = checkAllRescale(Text, temp)
+        # image_copy.paste(Text, findCenterXY(temp.CENTER_BOTTOM, temp))
+
+        OuterLayer.save("Output/Image_Flip/" + str(i) + ".png")
+
+        OuterLayer.close()
+        IMG.close()
+        LeftArrow.close()
+        RightArrow.close()
+
+
+# Image Flip with preview
+def imageFlip():
+    session = [dirFiles("OuterBox"), dirFiles("Image"), dirFiles("LeftArrow"), dirFiles("RightArrow"),
+               dirFiles("GallaryBottom")]
+    que_List = list(itertools.product(*session))
+    # print(que_List)
+
+    for i in range(0, 200):
+        print(i)
+        rand = random.randint(1, len(que_List))
+
+        try:
+            OuterBox = Image.open(que_List[rand][0])
+            IMG = Image.open(que_List[rand][1])
+            LeftArrow = Image.open(que_List[rand][2])
+            LeftArrow.thumbnail((200, 200))
+
+            RightArrow = Image.open(que_List[rand][3])
+            RightArrow.thumbnail((200, 200))
+
+            GallaryBottom = Image.open(que_List[rand][4])
+            GallaryBottom.thumbnail((400, 1000))
+
+        except IOError:
+            print("Error")
+
+        # Initial the Outer Box
+        temp = ImageCons(OuterBox.size)
+        temp.setBase(OuterBox.size[0], OuterBox.size[1])
+        OuterLayer = OuterBox.copy()
+
+        # Process IMG
+        temp.setTarget(IMG.size[0], IMG.size[1])
+        # IMG = scaleByRatio(IMG, temp, 0.6)
+        IMG = checkAllRescale(IMG, temp)
+        OuterLayer.paste(IMG, findCenterXY(temp.CENTER, temp))
+
+        print(LeftArrow.size)
+        temp.setTarget(LeftArrow.size[0], LeftArrow.size[1])
+        # LeftArrow = checkAllRescale(LeftArrow, temp)
+        # GallaryBottom = scaleByRatio(GallaryBottom, temp, 1.3)
+        OuterLayer.paste(LeftArrow, findCenterXY(temp.LEFT, temp))
+
+        print(LeftArrow.size)
+        temp.setTarget(RightArrow.size[0], RightArrow.size[1])
+        # RightArrow = checkAllRescale(RightArrow, temp)
+        # GallaryBottom = scaleByRatio(GallaryBottom, temp, 1.3)
+        OuterLayer.paste(RightArrow, findCenterXY(temp.RIGHT, temp))
+
+        temp.setTarget(GallaryBottom.size[0], GallaryBottom.size[1])
+        # GallaryBottom = checkAllRescale(GallaryBottom, temp)
+        # GallaryBottom = scaleByRatio(GallaryBottom, temp, 1.4)
+        OuterLayer.paste(GallaryBottom, findCenterXY(temp.CENTER_BOTTOM, temp))
+
+        OuterLayer.save("Output/ImageFlipWithPreview/" + str(i) + ".png")
+
+        OuterLayer.close()
+        IMG.close()
+        LeftArrow.close()
+        RightArrow.close()
+        GallaryBottom.close()
+
+
+# List
+# Header
+# Footer
+# Text
+
+imageFlip()
+# plainImageGallary()
+# structRightImageBox()
 # structLeftImageBox()
+# structCenterImageBox()
---
+++ App/src/ImageDatasetCreation/MergeTool.py
@@ -0,0 +1,77 @@
+import os
+import random
+
+from PIL import Image
+
+TARGET_WIDTH = 0
+TARGET_HEIGHT = 0
+
+
+def getRandom():
+    RANDOM_SHIFT_PIXAL = 10
+    plusnegative = random.randint(0, 1)
+    if plusnegative == 1:
+        return random.randint(1, RANDOM_SHIFT_PIXAL)
+    else:
+        return -random.randint(1, RANDOM_SHIFT_PIXAL)
+
+
+def dirFiles(itemType):
+    path = "Assets/" + itemType
+    lst = []
+    for item in os.listdir(path):
+        if not item.startswith('.') and os.path.isfile(os.path.join(path, item)):
+            lst.append(item)
+
+    # lst = os.listdir(path)
+    for i in range(0, len(lst)):
+        temp = lst[i]
+        lst[i] = path + "/" + temp
+    return lst
+
+
+def findCenterXY(snapCorner, base):
+    # print(base.getT_H())
+    # print(int(snapCorner[0] - base.getT_W() / 2), int(snapCorner[1] - base.getT_H() / 2))
+    return int(snapCorner[0] - base.getT_W() / 2), int(snapCorner[1] - base.getT_H() / 2)
+
+
+def checkAllRescale(img, base):
+    width_thres = int(base.getB_W() / 6)
+    height_thres = int(base.getB_H() / 6)
+    if img.size[0] > width_thres or img.size[1] > height_thres:
+        wpercent = (width_thres / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((width_thres, hsize), Image.ANTIALIAS)
+        base.setTarget(width_thres, hsize)
+        return img
+
+
+def checkHeightRescale(img, base):
+    width_thres = int(base.getB_W() / 5)
+    height_thres = int(base.getB_H() / 5)
+    if img.size[1] > height_thres:
+        wpercent = (width_thres / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((width_thres, hsize), Image.ANTIALIAS)
+        base.setTarget(width_thres, hsize)
+        return img
+
+
+def checkWidthRescale(img, base):
+    width_thres = int(base.getB_W() / 5)
+    height_thres = int(base.getB_H() / 5)
+    if img.size[0] > width_thres:
+        wpercent = (width_thres / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((width_thres, hsize), Image.ANTIALIAS)
+        base.setTarget(width_thres, hsize)
+        return img
+
+
+def scaleByRatio(img, base, ratio):
+    img = img.resize((int(int(img.size[0]) * ratio), int(int(img.size[1]) * ratio)), Image.ANTIALIAS)
+    base.setTarget(int(int(img.size[0]) * ratio), int(int(img.size[1]) * ratio))
+    return img
+
+
