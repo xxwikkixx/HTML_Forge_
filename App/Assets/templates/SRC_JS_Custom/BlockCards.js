23c6840e72a5eec79e81c92f6a2b605e8df86f07
Khalid Q <khalkmq@gmail.com>
2019-04-25 23:23:48 -0400

    Modals Added, Removed Unused Pages

--- App/Assets/templates/SRC_JS_Custom/BlockCards.js
+++ App/Assets/templates/SRC_JS_Custom/BlockCards.js
@@ -232,6 +232,8 @@ function isValidQueue(){
     // Check for unrecognized labels in queue
     for(var i = 0; i < BLOCK_QUEUE.length; i++){
         if(BLOCK_QUEUE[i] == "Not Recognized" ) {
+            document.getElementById('detErr').style.display = "block";
+            document.getElementById('detErr2').style.display = "block";
             $('#Generate-btn').prop('disabled', true);
             return false;
         }
